632642b22a06bdbd231269246522789ecffa0505
Petrovich1973 <designtorg@ya.ru>
2020-08-13 21:07:28 +0300

    decoration table

--- spa/src/pages/RoleDetail.js
+++ spa/src/pages/RoleDetail.js
@@ -40,6 +40,25 @@ const RoleDetail = () => {
         // eslint-disable-next-line
     }, [])
 
+    const renderCels = (keyTask, task) => {
+        return (
+            <>
+                <td>
+                    <nobr>
+                        {keyTask === '*' ?
+                            'for all classes' :
+                            keyTask}
+                    </nobr>
+                </td>
+                <td>
+                    {task.map((cache, idCache) => (
+                        <label key={idCache}>{cache}</label>
+                    ))}
+                </td>
+            </>
+        )
+    }
+
     return (
         <section className="align-center">
 
@@ -49,42 +68,38 @@ const RoleDetail = () => {
             {dfltAll && <div><p>Super Role</p></div>}
             {isAllEmpty && <div><p>all rights, does not change</p></div>}
             {role && !dfltAll && !isAllEmpty && <table className="table_detail_role">
-                <tbody>
+
                 {Object.keys(permissions).map((key, i) => {
                     const perm = permissions[key]
                     const rowSpan = perm && Object.keys(perm).length
                     return (
-                        <tr key={i}>
-                            <td rowSpan={1}><strong>{key}</strong></td>
-                            <td>
-                                <table>
-                                    <tbody>
-                                    {perm && Object.keys(perm).map((keyTask, idTask) => {
-                                        const task = perm[keyTask]
-                                        return (
-                                            <tr key={idTask}>
-                                                <td>
-                                                    <nobr>
-                                                        {keyTask === '*' ?
-                                                            'for all classes' :
-                                                            keyTask}
-                                                    </nobr>
-                                                </td>
-                                                <td>
-                                                    {task.map((cache, idCache) => (
-                                                        <label key={idCache}>{cache}</label>
-                                                    ))}
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                    </tbody>
-                                </table>
-                            </td>
+                        <tbody key={i}>
+                        <tr>
+                            <td rowSpan={rowSpan || 1}><strong>{key}</strong></td>
+                            {perm && !rowSpan && <>
+                                <td colSpan={2}/>
+                            </>}
+                            {perm && rowSpan < 2 && Object.keys(perm).slice(0, 1).map((keyTask, idTask) => {
+                                const task = perm[keyTask]
+                                return (
+                                    <React.Fragment key={idTask}>
+                                        {renderCels(keyTask, task)}
+                                    </React.Fragment>
+                                )
+                            })}
                         </tr>
+                        {perm && rowSpan > 1 && Object.keys(perm).slice(1).map((keyTask, idTask) => {
+                            const task = perm[keyTask]
+                            return (
+                                <tr key={idTask}>
+                                    {renderCels(keyTask, task)}
+                                </tr>
+                            )
+                        })}
+                        </tbody>
                     )
                 })}
-                </tbody>
+
             </table>}
         </section>
     )
